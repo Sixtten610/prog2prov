a94b3b8bee0de36070757c7a486024a98f9a5ba4
6_10 <60339623+Sixtten610@users.noreply.github.com>
2020-10-19 21:48:32 +0200

    t

---
+++ prog2prov/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace prog2prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
